22a4a0619106a8120678e594f8a6d6f854fce8ef
chpKrishnamohan-gh <chpkrishnamohan.com>
2026-01-15 14:49:53 +0530

    wiping everything

--- saroi-react/src/HomePage/MainBody/ImageContainer.jsx
+++
@@ -1,9 +0,0 @@
-function ImageContainer({ imageUrl }) {
-  return (
-    <div className="result-image-container">
-      <img src={imageUrl} alt="Result" className="result-image" />
-    </div>
-  );
-}
-
-export default ImageContainer;
